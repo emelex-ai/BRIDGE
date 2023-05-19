4d44bc17ad190b8283b8e7273c7f1f5d939b843d
Nathan Crock <mathnathan@gmail.com>
2023-05-19 19:28:31 -0400

    Moving code base from Google Colab to a local machine.

---
+++ train.py
@@ -0,0 +1,166 @@
+from dataset import ConnTextULDataset
+from model import Model
+import torch as pt
+import wandb
+import tqdm
+import sys
+import math
+import glob
+wandb.login()
+
+MODEL_PATH = './models'
+ds = ConnTextULDataset()
+
+if pt.cuda.is_available():
+   device = pt.device('cuda:0')
+else:
+   device = pt.device('cpu')
+
+device = 'cpu'
+print("device = ", device)
+
+exp_count = 1
+num_epochs = 100
+CONTINUE = False
+learning_rate = 1e-3
+batch_size = 32
+train_test_split = 0.8
+cutpoint = int(train_test_split * len(ds))
+d_model= 128
+nhead = 4
+assert d_model%nhead == 0, "d_model must be evenly divisible by nhead"
+
+train_dataset_slices = []
+for batch in range(math.ceil(cutpoint/batch_size)):
+  train_dataset_slices.append(slice(batch*batch_size, min((batch+1)*batch_size, cutpoint)))
+
+val_dataset_slices = []
+for batch in range(math.ceil((len(ds)-cutpoint)/batch_size)):
+  val_dataset_slices.append(slice(cutpoint+batch*batch_size, min(cutpoint+(batch+1)*batch_size, len(ds))))
+
+# Get latest model run information
+model_runs = glob.glob(MODEL_PATH+'/*')
+if model_runs:
+  latest_run = sorted(model_runs)[-1].split('/')[-1]
+  model_id, epoch_num = int(latest_run.split("_")[0][5:]), int(latest_run.split("_")[-1].split('.')[0][10:])
+  if not CONTINUE:
+    model_id += 1
+    epoch_num = 0
+else:
+  model_id, epoch_num = 0, 0
+
+# A number for WandB:
+n_steps_per_epoch = len(train_dataset_slices)
+
+# Launch `exp_count` experiments, trying different dropout rates
+for _ in range(exp_count):
+
+    if CONTINUE:
+      chkpt = pt.load(MODEL_PATH+f"/model{model_id}_checkpoint{epoch_num}.pth")
+      model = Model(len(ds.character_tokenizer), len(ds.phonology_tokenizer), d_model=chkpt['d_model'], nhead=chkpt['nhead'])
+      model.load_state_dict(chkpt['model'])
+      opt = pt.optim.AdamW(model.parameters(), learning_rate)
+      opt.load_state_dict(chkpt['optimizer'])
+    else:
+      model = Model(len(ds.character_tokenizer), len(ds.phonology_tokenizer), d_model=d_model, nhead=nhead)
+      opt = pt.optim.AdamW(model.parameters(), learning_rate)
+
+    # 🐝 initialise a wandb run
+    run = wandb.init(
+              project="ConnTextUL_WandB",
+              config={
+                  "starting_epoch": epoch_num,
+                  "epochs": num_epochs,
+                  "batch_size": batch_size,
+                  'd_model': d_model,
+                  'nhead': nhead,
+                  "lr": learning_rate,
+                  "id": model_id
+                  })
+    
+    generated_text_table = wandb.Table(columns=["Step", "Generated Output"])
+    
+    # Copy your config 
+    config = run.config
+
+    pbar = tqdm.tqdm(range(epoch_num, epoch_num+num_epochs), position=0)
+
+    run.watch(model, log='all')
+
+    model.to(device)
+
+    #Training
+    example_ct = 0
+    for epoch in pbar:
+        model.train()
+        print("\nTraining Loop...")
+        for step, batch_slice in enumerate(train_dataset_slices):
+            batch = ds[batch_slice]
+            orthography, phonology = batch['orthography'].to(device), batch['phonology'].to(device)
+            logits = model(orthography['enc_input_ids'], orthography['enc_pad_mask'],
+                        orthography['dec_input_ids'], orthography['dec_pad_mask'],
+                        phonology['enc_input_ids'], phonology['enc_pad_mask'],
+                        phonology['dec_input_ids'], phonology['dec_pad_mask'])
+                      
+            orth_loss = pt.nn.CrossEntropyLoss(ignore_index=4)(logits['orth'], orthography['enc_input_ids'][:,1:]) 
+            phon_loss = pt.nn.CrossEntropyLoss(ignore_index=2)(logits['phon'], phonology['targets'])
+            loss = orth_loss + phon_loss
+
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+
+            # Now we generate orthographic tokens and phonological vectors for the input 'elephant'
+            if 1:
+              orth = ds.character_tokenizer.encode(['elephant'])
+              orthography = orth['enc_input_ids'].to(device)
+              orthography_mask = orth['enc_pad_mask'].to(device)
+              phon = ds.phonology_tokenizer.encode(['elephant'])
+              phonology = [[t.to(device) for t in tokens] for tokens in phon['enc_input_ids']]
+              phonology_mask = phon['enc_pad_mask'].to(device)
+              generation = model.generate(orthography, orthography_mask, phonology, phonology_mask)
+              generated_text = ds.character_tokenizer.decode(generation['orth'].tolist())[0]
+              # Log the text in the WandB table
+              generated_text_table.add_data(step, generated_text)
+
+            example_ct += len(batch['orthography'])
+            metrics = {"train/orth_loss": orth_loss,
+                       "train/phon_loss": phon_loss,
+                       "train/train_loss": loss, 
+                       "train/epoch": epoch, 
+                       "train/example_ct": example_ct,
+                       "train/global_embedding_magnitude": pt.norm(model.global_embedding[0], p=2),
+                       "train/model_weights_magnitude": pt.sqrt(sum([pt.norm(w[0],p=2)**2 for w in model.parameters()])),
+                       #"train/lr": opt.state_dict()['param_groups'][0]['lr'],
+                       "train/generated_text_table": generated_text_table
+                      }
+            run.log(metrics)
+
+        model.eval()
+        with pt.no_grad():
+          print("\nValidation Loop...")
+          for step, batch_slice in enumerate(val_dataset_slices):
+              batch = ds[batch_slice]
+              orthography, phonology = batch['orthography'].to(device), batch['phonology'].to(device)
+              logits = model(orthography['enc_input_ids'], orthography['enc_pad_mask'],
+                          orthography['dec_input_ids'], orthography['dec_pad_mask'],
+                          phonology['enc_input_ids'], phonology['enc_pad_mask'],
+                          phonology['dec_input_ids'], phonology['dec_pad_mask'])
+            
+              val_loss = pt.nn.CrossEntropyLoss(ignore_index=4)(logits['orth'] , orthography['enc_input_ids'][:,1:]) 
+              val_loss = val_loss + pt.nn.CrossEntropyLoss(ignore_index=2)(logits['phon'], phonology['targets'])              
+              more_metrics = {"val/val_loss": val_loss}             
+              run.log(more_metrics)
+        
+        pt.save({
+          'epoch': epoch,
+          'batch_size': batch_size,
+          'd_model': d_model,
+          'nhead': nhead,
+          'model': model.state_dict(),
+          'optimizer': opt.state_dict()
+        },
+        MODEL_PATH + f"/model{model_id}_checkpoint{epoch}.pth")
+
+# 🐝 Close your wandb run 
+run.finish()
