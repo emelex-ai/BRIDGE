d4834a090bc1b6509087903a7dc938e053ab873b
Nathan Crock <mathnathan@gmail.com>
2023-06-04 21:14:41 -0400

    Update Accuracy Calculations to use Masking Fixes #27

--- train_impl.py
+++ train_impl.py
@@ -83,82 +83,68 @@ def single_step(c, pbar, model, train_dataset_slices, batch_slice, ds, device, o
     metrics = compute_metrics(logits, orthography, phonology, batch, example_ct, orth_loss, phon_loss, loss, epoch, step, ds, device, model, generated_text_table, mode)
     return metrics
 
-#----------------------------------------------------------------------
-def compute_metrics(logits,orthography, phonology, batch, example_ct, orth_loss, phon_loss, loss, epoch, step, ds, device, model, generated_text_table, mode):
+def calculate_accuracies(logits, orthography, phonology):
 
-    # --- Calculte Orthographic Accuracy ---
+    # --- Calculate Orthographic Accuracy ---
     # Determine model predictions by taking argmax of orthographic logits
     orth_pred = pt.argmax(logits["orth"], dim=1)
     # Use the orthographic encoder input ids as labels
     # notice we slice from 1: onwards to remove the start token (not predicted)
-    orth_true = orthography["enc_input_ids"][:, 1:]
-    # We wish to exclude the padding tokens from the letter-wise accuracy calculation.
-    # to ensure that they do not contribute to the loss we find the location of the 4's
-    # in orth_true, and set the corresponding locations in orth_pred to -1. This ensures
-    # that they will be incorrect, and we can then simply subtract the total number of 
-    # 4's from the denominator of the accuracy calculation to exclude them.
-    four_locs = pt.where(orth_true == 4) # Find the locations of the padding tokens (4)
-    num_fours = (orth_true == 4).sum() # keep track of how many 4's there are
-    orth_pred[four_locs] = -1 # Set location of padding tokens to -1
-    
-    # Create a boolean tensor where each element indicates a correct or incorrect prediction
-    # This includes the padding tokens (all padding location tokens are now guaranteed 
-    # to be incorrect)
-    orth_matches = orth_pred == orth_true 
-    letter_wise_accuracy = (orth_matches).sum()/(pt.tensor(orth_pred.shape).prod()-num_fours)
+    orth_true = orthography['enc_input_ids'][:,1:]
+
+    # Create a mask for valid positions (not padding)
+    orth_valid_mask = (orth_true != 4)
+
+    # Apply the mask to true and predicted values
+    masked_orth_true = orth_true[orth_valid_mask]
+    masked_orth_pred = orth_pred[orth_valid_mask]
+
+    # Calculate letter-wise accuracy
+    correct_matches = (masked_orth_pred == masked_orth_true).sum()
+    letter_wise_accuracy = correct_matches.float() / orth_valid_mask.sum().float()
 
     # To calculate word-wise accuracy, we need to check if all letters in a single word are correct
-    # for this calculation, we need to ensure all the padding tokens are correctly predicted so 
-    # that their potential falseness does not affect the word-wise accuracy calculation.
-    orth_pred[four_locs] = 4 # Set location of padding tokens to 4. Now all padding predictions are correct.
-    orth_matches = orth_pred == orth_true 
-    # Accuracy is now calculated by checking if all letters in a word are correct, summing, 
-    # and dividing by the number of words.
-    orth_word_accuracy = (orth_matches).all(dim=1).sum()/orth_pred.shape[0]
+    orth_pred[~orth_valid_mask] = 4
+    word_wise_mask = (orth_pred == orth_true)
+    orth_word_accuracy = word_wise_mask.all(dim=1).float().mean()
 
-    # --- Calculte Phonological Accuracy ---
-    # For phonemes, we also need to exclude the padding tokens from the accuracy calculation.
-    # However, this time it is more simple because the padding tokens are set to 2, which is 
-    # not within the range of predictions for the model, therefore our model will always get the
-    # padding tokens wrong. We can simply exclude them from the accuracy calculation by subtracting
-    # the number of padding tokens from the denominator of the accuracy calculation.
-
-    # There are three ways to calculate accuracy with the phonological predictions:
-    # 1. Phoneme segment accuracy: Accuracy of each phoneme segment across all phonological vectors
-    # 2. Phoneme-wise accuracy: Accuracy of each phonological vector across all words
-    # 3. Phoneme word accuracy: Accuracy of each word across the entire batch
-
-    # Take argmax of phonological logits to determine model predictions
+    # --- Calculate Phonological Accuracy ---
     phon_pred = pt.argmax(logits["phon"], dim=1)
-    phon_true = phonology["targets"]
+    phon_true = phonology['targets']
 
-    # 1. Phoneme segment accuracy: Accuracy of each phoneme segment across all phonological vectors
-    phon_matches = (phon_pred == phon_true)
-    num_twos = (phon_true == 2).sum()
-    phon_segment_accuracy = phon_matches.sum()/(pt.tensor(phon_pred.shape).prod()-num_twos)
+    # Create a mask for valid positions (not padding)
+    phon_valid_mask = (phon_true != 2)
 
-    # 2. Phoneme-wise accuracy: Accuracy of each phonological vector across all words
-    # Each padding phononological vector is filled with 2's. So we can count the number 
-    # of padding phonological vectors by dividing the total number of 2's by the length
-    # of the phonological vector.
-    num_padding_vectors = num_twos/phon_true.shape[-1]
-    # Begin by checking if all phoneme segments are correct along each phonological vector (dim=-1)
-    # Then, rather than divide by the product of all dimensions as before, we divide by the product
-    # of the batch dimension (num words) and the max_phon_length dimension (num phonological vectors)
-    # and subtract the number of padding vectors, because those are guaranteed to be incorrect.
-    phoneme_wise_accuracy = (phon_matches).all(dim=-1).sum()/(pt.tensor(phon_pred.shape[:2]).prod()-num_padding_vectors)
+    # Apply the mask to true and predicted values
+    masked_phon_true = phon_true[phon_valid_mask]
+    masked_phon_pred = phon_pred[phon_valid_mask]
 
-    # 3. Phoneme word accuracy: Accuracy of each word across the entire batch
-    # To use the .all() method for determining word accuracy we must set all the elements 
-    # in phon_pred that correspond to padding in the phon_true, to 2.
-    two_locs = pt.where(phon_true == 2) # Find the locations of the padding tokens (2)
-    phon_pred[two_locs] = 2 # Set location of padding tokens to 2 (all padding predictions are now correct)
-    # Accuracy is now calculated by checking if all phonological vectors in a word are correct, summing, 
-    # and dividing by the number of words, i.e. the batch_size.
-    phon_matches = (phon_pred == phon_true)
-    phon_word_accuracy = (phon_matches).all(dim=-1).all(dim=-1).sum()/phon_pred.shape[0]
+    # Phoneme segment accuracy
+    correct_phoneme_segments = (masked_phon_pred == masked_phon_true).sum()
+    phon_segment_accuracy = correct_phoneme_segments.float() / phon_valid_mask.sum().float()
 
-    # Now we generate orthographic tokens and phonological vectors for the input 'elephant'
+    # Phoneme-wise accuracy
+    phoneme_wise_mask = (phon_pred == phon_true)
+    phoneme_wise_accuracy = phoneme_wise_mask.all(dim=-1).sum()/(masked_phon_true.shape[0]/phon_true.shape[-1])
+
+    # Phoneme word accuracy
+    word_accuracies = [word[target != 2].all().int() for word, target in zip(phoneme_wise_mask, phon_true)]
+    #phon_word_accuracy = phoneme_wise_mask.all(dim=-1).all(dim=-1).sum()/phon_true.shape[0]
+    phon_word_accuracy = sum(word_accuracies)/len(word_accuracies)
+
+    output = {"letter_wise_accuracy": letter_wise_accuracy,
+              "word_wise_accuracy": orth_word_accuracy,
+              "phon_segment_accuracy": phon_segment_accuracy,
+              "phoneme_wise_accuracy": phoneme_wise_accuracy,
+              "phon_word_accuracy": phon_word_accuracy}
+    
+    return output
+
+
+#----------------------------------------------------------------------
+def compute_metrics(logits,orthography, phonology, batch, example_ct, orth_loss, phon_loss, loss, epoch, step, ds, device, model, generated_text_table, mode):
+
+
     if 0:
         orth = ds.character_tokenizer.encode(["elephant"])
         orthography = orth["enc_input_ids"].to(device)
@@ -194,17 +180,11 @@ def compute_metrics(logits,orthography, phonology, batch, example_ct, orth_loss,
         ),
         # "train/lr": opt.state_dict()['param_groups'][0]['lr'],
         mode+"/generated_text_table": generated_text_table,
-        mode+"/letter_wise_accuracy": letter_wise_accuracy,
-        mode+"/orth_word_accuracy": orth_word_accuracy,
-        mode+"/phon_segment_accuracy": phon_segment_accuracy,
-        mode+"/phoneme_wise_accuracy": phoneme_wise_accuracy,
-        mode+"/phon_word_accuracy": phon_word_accuracy,
     }
-    #print(f"letter_wise_accuracy: {letter_wise_accuracy}")
-    #print(f"orth_word_accuracy: {orth_word_accuracy}")
-    #print(f"phon_segment_accuracy: {phon_segment_accuracy}")
-    #print(f"phoneme_wise_accuracy: {phoneme_wise_accuracy}")
-    #print(f"phon_word_accuracy: {phon_word_accuracy}")
+
+    accuracies = calculate_accuracies(logits, orthography, phonology)
+    for accuracy in accuracies:
+        metrics[mode+"/"+accuracy] = accuracies[accuracy]
 
     return metrics
 #----------------------------------------------------------------------
