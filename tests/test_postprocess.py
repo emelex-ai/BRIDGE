c9d1f3a94a2fe5e09966474bf31451f064f34c60
mathnathan <mathnathan@gmail.com>
2024-08-23 18:25:08 +0000

    Added some comments and thoughts around future type hints and pydantic models

--- tests/test_postprocess.py
+++ tests/test_postprocess.py
@@ -3,14 +3,27 @@ import pickle
 from collections import Counter
 from data.preprocess import input_data
 
+# We may consider moving the WordData class from the
+# preprocess.py file to a separate file and importing it
+# here as well. In fact, in the future we may benefit from
+# refactoring and using pydantic models for data validation.
+# Particularly, because there is a massive "config" object
+# that is passed around in the model right now. This could
+# be a good candidate for a pydantic model.
+
 
 def validate_input_data(data, word_count):
+    # Perhaps we can think about the phoneme_feature_size
+    # a parameter in the config.yaml file. Matt and
+    # Nathan were discussing adding new phoneme features
+    # to expand language and dialect support.
     phoneme_feature_size = 31
     for word, v in data.items():
         assert v["count"] == word_count[word]
         assert max(v["phoneme"][0]) + 1 == v["phoneme_shape"][0]
         assert v["phoneme_shape"][1] == phoneme_feature_size
         assert len(v["orthograph"]) == len(word)
+
 
 def test_input_data():
     df = pd.read_csv("data/data.csv")
