96bc4381867d1f982c497ea5940b2628e8ef3ea4
Giulio Martini <giulio.martini@getrightdata.com>
2024-11-16 01:25:42 -0500

    finished

--- latent_scope_implementation/ds_prepper.py
+++ latent_scope_implementation/ds_prepper.py
@@ -1,19 +1,30 @@
 import pandas as pd
 import h5py
 import numpy as np
+import sys
+import os
+
+module_path = os.environ["LATENT_SCOPE_PATH"]
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import latentscope as ls
+
 
 class DSPrepper:
 
-    def __init__(self, dataset_path: str, output_path: str):
+    def __init__(self, dataset_path: str, output_path: str, latent_scope_project_name : str):
         self.dataset_path = dataset_path
         self.output_path = output_path
         self.dataset = None
+        self.latent_scope_project_name = latent_scope_project_name
 
     def load_and_process(self):
         self.dataset = pd.read_csv(self.dataset_path)
         self._transform_encodings()
         self.dataset.to_csv(self.output_path)
         print("Saved csv file")
+        return self.dataset
 
     def _transform_encodings(self):
         def parse_encoding(x):
@@ -21,11 +32,29 @@ class DSPrepper:
                 return [float(value) for value in x.split(":")]
             except ValueError:
                 return None
+        
         self.dataset["global_encoding"] = self.dataset["global_encoding"].apply(parse_encoding)
         self.dataset.dropna(subset=["global_encoding"], inplace=True)
+        self.dataset["global_encoding"] = self.dataset["global_encoding"].apply(
+            lambda x: np.array(x, dtype=np.float32)
+        )
         self.dataset = self.dataset[["word_raw", "global_encoding"]]
+
+    def ingest_file(self, dataset):
+        ls.init(module_path)
+        print("--- --- --- INITIALIZED --- --- -- ")
+        print(f"PROJECT NAME: {self.latent_scope_project_name}")
+        ls.ingest(self.latent_scope_project_name, dataset, "global_encoding")
+        print("--- --- --- INGESTED --- --- --- ")
+        embeddings = np.array(dataset["global_encoding"].to_list())
+        print("--- --- --- --- --- --- --- --- --- ---")
+        ls.import_embeddings(self.latent_scope_project_name, np.array(dataset["global_encoding"].to_list()).reshape(-1, embeddings.shape[1]), text_column="global_encoding", model_id = "")
+        ls.serve()
+
 
     def convert_to_h5(self, h5_path: str):
         embeddings = np.array(self.dataset["global_encoding"].tolist(), dtype=np.float32)
         with h5py.File(h5_path, "w") as hf:
             hf.create_dataset("embeddings", data=embeddings)
+
+
--- latent_scope_implementation/latent_scope_starter.py
+++ latent_scope_implementation/latent_scope_starter.py
@@ -1,12 +1,21 @@
 import argparse
+import sys
+import os
 from ds_prepper import DSPrepper
+
+module_path = os.environ["LATENT_SCOPE_PATH"]
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import latentscope as ls
 
 class LSStarter:
 
-    def __init__(self, dataset_path: str, output_path: str, h5_path: str):
-        prepper = DSPrepper(dataset_path, output_path)
-        prepper.load_and_process()
+    def __init__(self, dataset_path: str, output_path: str, h5_path: str, directory_name: str):
+        prepper = DSPrepper(dataset_path, output_path, directory_name)
+        dataset = prepper.load_and_process()
         print("Saved h5 file")
+        prepper.ingest_file(dataset)
         prepper.convert_to_h5(h5_path)
 
 def main():
@@ -30,9 +39,16 @@ def main():
         help="Path to save the HDF5 file."
     )
 
+    parser.add_argument(
+        "--directory_name",
+        type=str,
+        required=True,
+        help="Name of latent scope resulting directory"
+    )
+
     args = parser.parse_args()
 
-    LSStarter(args.dataset_path, args.output_path, args.h5_path)
+    LSStarter(args.dataset_path, args.output_path, args.h5_path, args.directory_name)
 
 if __name__ == "__main__":
     main()
