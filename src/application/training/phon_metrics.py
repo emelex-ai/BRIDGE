b50d9b46df2a3ea17ec2c0d7402428e8785eea2e
Eric Sharkey <eric.r.sharkey@gmail.com>
2024-11-20 01:32:34 +0000

    In progress cosine

---
+++ src/application/training/phon_metrics.py
@@ -0,0 +1,63 @@
+import torch
+
+
+def calculate_phon_word_accuracy(phon_true, phoneme_wise_mask):
+    word_accuracies = [
+        word[target != 2].all().int()
+        for word, target in zip(phoneme_wise_mask, phon_true)
+    ]
+    phon_word_accuracy = sum(word_accuracies) / len(word_accuracies)
+    return phon_word_accuracy
+
+ 
+def calculate_phoneme_wise_accuracy(phon_true, masked_phon_true, phoneme_wise_mask):
+    return phoneme_wise_mask.all(dim=-1).sum() / (
+        masked_phon_true.shape[0] / phon_true.shape[-1]
+    )
+
+
+def calculate_phon_feature_accuracy(
+    phon_valid_mask, masked_phon_true, masked_phon_pred
+):
+    correct_features = (masked_phon_pred == masked_phon_true).sum()
+    phon_feature_accuracy = correct_features.float() / phon_valid_mask.sum().float()
+
+    return phon_feature_accuracy
+
+def calculate_cosine_distance(
+    phon_true: torch.Tensor, phon_pred: torch.Tensor
+):
+    cosine_sims = torch.Tensor()
+    for i in range(phon_true.shape[0]):
+        true = phon_true[i].type(torch.float)
+        pred = phon_pred[i].type(torch.float)
+        mask = true != 2
+        true_masked = true[mask].reshape((-1,true.shape[1]))
+        pred_masked = pred[mask].reshape((-1,true.shape[1]))
+        f = torch.nn.CosineSimilarity(dim=1)
+        cosine_sims = torch.cat([cosine_sims, f(pred_masked, true_masked)])
+    return torch.mean(cosine_sims)
+
+def calculate_phon_metrics(
+    logits: dict[str, torch.Tensor], phonology: dict[str, torch.Tensor]
+) -> dict[str, float]:
+    phon_pred = torch.argmax(logits["phon"], dim=1)
+    phon_true = phonology["targets"]
+    phon_valid_mask = phon_true != 2
+    masked_phon_true = phon_true[phon_valid_mask]
+    masked_phon_pred = phon_pred[phon_valid_mask]
+    cosine_accuracy = calculate_cosine_distance(phon_true, phon_pred)
+    phon_feature_accuracy = calculate_phon_feature_accuracy(
+        phon_valid_mask, masked_phon_true, masked_phon_pred
+    )
+    phoneme_wise_mask = phon_pred == phon_true
+    phoneme_wise_accuracy = calculate_phoneme_wise_accuracy(
+        phon_true, masked_phon_true, phoneme_wise_mask
+    )
+    phon_word_accuracy = calculate_phon_word_accuracy(phon_true, phoneme_wise_mask)
+    return {
+        "phon_cosine_accuracy": cosine_accuracy.item(),
+        "phon_feature_accuracy": phon_feature_accuracy.item(),
+        "phon_phoneme_wise_accuracy": phoneme_wise_accuracy.item(),
+        "phon_word_accuracy": phon_word_accuracy,
+    }
--- src/application/training/training_pipeline.py
+++ src/application/training/training_pipeline.py
@@ -1,6 +1,8 @@
 import torch
 from tqdm import tqdm
 from typing import Dict, Any
+from src.application.training.ortho_metrics import calculate_orth_metrics
+from src.application.training.phon_metrics import calculate_phon_metrics
 from src.domain.datamodels import TrainingConfig
 from src.domain.dataset import BridgeDataset
 from src.domain.model import Model
@@ -116,53 +118,20 @@ class TrainingPipeline:
         orthography: Dict[str, torch.Tensor],
         phonology: Dict[str, torch.Tensor],
     ) -> dict:
-
+        metrics = {}
         if self.training_config.training_pathway in ["o2p", "op2op", "p2p"]:
-            phon_pred = torch.argmax(logits["phon"], dim=1)
-            phon_true = phonology["targets"]
-            phon_valid_mask = phon_true != 2
-            masked_phon_true = phon_true[phon_valid_mask]
-            masked_phon_pred = phon_pred[phon_valid_mask]
-            correct_features = (masked_phon_pred == masked_phon_true).sum()
-            phon_feature_accuracy = (
-                correct_features.float() / phon_valid_mask.sum().float()
-            )
-            phoneme_wise_mask = phon_pred == phon_true
-            phoneme_wise_accuracy = phoneme_wise_mask.all(dim=-1).sum() / (
-                masked_phon_true.shape[0] / phon_true.shape[-1]
-            )
-            word_accuracies = [
-                word[target != 2].all().int()
-                for word, target in zip(phoneme_wise_mask, phon_true)
-            ]
-            phon_word_accuracy = sum(word_accuracies) / len(word_accuracies)
-
-            metrics = {
-                "phon_feature_accuracy": phon_feature_accuracy.item(),
-                "phon_phoneme_wise_accuracy": phoneme_wise_accuracy.item(),
-                "phon_word_accuracy": phon_word_accuracy,
-            }
+            metrics.update(calculate_phon_metrics(logits, phonology))
 
         if self.training_config.training_pathway in ["op2op", "p2o"]:
-            orth_pred = torch.argmax(logits["orth"], dim=1)
-            orth_true = orthography["enc_input_ids"][:, 1:]
-            orth_valid_mask = orth_true != 4
-            masked_orth_true = orth_true[orth_valid_mask]
-            masked_orth_pred = orth_pred[orth_valid_mask]
-            correct_matches = (masked_orth_pred == masked_orth_true).sum()
-            letter_wise_accuracy = (
-                correct_matches.float() / orth_valid_mask.sum().float()
-            )
-            orth_pred[~orth_valid_mask] = 4
-            word_wise_mask = orth_pred == orth_true
-            orth_word_accuracy = word_wise_mask.all(dim=1).float().mean()
-
-            metrics = {
-                "letter_wise_accuracy": letter_wise_accuracy.item(),
-                "word_wise_accuracy": orth_word_accuracy.item(),
-            }
+            metrics.update(calculate_orth_metrics(logits, orthography))
 
         return metrics
+
+
+
+    
+
+    
 
     def single_step(self, batch_slice: slice) -> Dict[str, Any]:
         batch = self.dataset[batch_slice]
