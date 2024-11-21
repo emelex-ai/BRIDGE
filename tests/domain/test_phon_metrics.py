0d08c0ebc438eb7877c142b1bf3ee000ce94f133
Eric Sharkey <eric.r.sharkey@gmail.com>
2024-11-21 23:56:56 +0000

    Fixed test

--- tests/domain/test_phon_metrics.py
+++ tests/domain/test_phon_metrics.py
@@ -4,9 +4,12 @@ import torch
 def test_cosine_distance():
     phon_pred = torch.load("tests/data/phon_pred.pt")
     phon_true = torch.load("tests/data/phon_pred.pt")
+    print(phon_pred - phon_true)
+    assert torch.sum(phon_pred-phon_true) == 0
     assert calculate_cosine_distance(phon_true, phon_pred).item() == 1.0
     
 def test_euclidean_distance():
     phon_pred = torch.load("tests/data/phon_pred.pt")
     phon_true = torch.load("tests/data/phon_pred.pt")
-    assert calculate_euclidean_distance(phon_true, phon_pred).item() - 5.744 < 0.1
+    assert torch.sum(phon_pred-phon_true) == 0
+    assert calculate_euclidean_distance(phon_true, phon_pred).item() < 0.01
