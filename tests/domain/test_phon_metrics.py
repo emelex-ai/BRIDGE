aad19575f18f02505c236410d664eb3d9260209f
Philippe Miron <philippemiron@gmail.com>
2024-11-22 01:43:25 +0000

    weights_only to remove warnings

--- tests/domain/test_phon_metrics.py
+++ tests/domain/test_phon_metrics.py
@@ -4,23 +4,23 @@ import torch
 
 
 def test_cosine_distance_identity():
-    phon_pred = torch.load("tests/data/phon_pred.pt")
-    phon_true = torch.load("tests/data/phon_pred.pt")
+    phon_pred = torch.load("tests/data/phon_pred.pt", weights_only=True)
+    phon_true = torch.load("tests/data/phon_pred.pt", weights_only=True)
     assert torch.sum(phon_pred - phon_true) == 0
     assert calculate_cosine_distance(phon_true, phon_pred).item() == 1.0
 
 def test_cosine_distance():
-    phon_pred = torch.load("tests/data/phon_pred.pt")
-    phon_true = torch.load("tests/data/phon_true.pt")
+    phon_pred = torch.load("tests/data/phon_pred.pt", weights_only=True)
+    phon_true = torch.load("tests/data/phon_true.pt", weights_only=True)
     assert math.isclose(calculate_cosine_distance(phon_true, phon_pred).item(), 0.242, rel_tol=1e-2)
 
 def test_euclidean_distance_identity():
-    phon_pred = torch.load("tests/data/phon_pred.pt")
-    phon_true = torch.load("tests/data/phon_pred.pt")
+    phon_pred = torch.load("tests/data/phon_pred.pt", weights_only=True)
+    phon_true = torch.load("tests/data/phon_pred.pt", weights_only=True)
     assert torch.sum(phon_pred - phon_true) == 0
     assert calculate_euclidean_distance(phon_true, phon_pred).item() < 0.01
 
 def test_euclidean_distance():
-    phon_pred = torch.load("tests/data/phon_pred.pt")
-    phon_true = torch.load("tests/data/phon_true.pt")
+    phon_pred = torch.load("tests/data/phon_pred.pt", weights_only=True)
+    phon_true = torch.load("tests/data/phon_true.pt", weights_only=True)
     assert math.isclose(calculate_euclidean_distance(phon_true, phon_pred).item(), 4.048, rel_tol=1e-2)
