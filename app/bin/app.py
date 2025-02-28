1d3357a7bed5f6d27b0b298ccfa8bacf7ce0180f
uzair-mughal <uzam.dev@gmail.com>
2025-02-28 00:16:24 -0500

    app update

--- app/bin/app.py
+++ app/bin/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 from src.application.interface import BridgeInference
 
 CHECKPOINT_PATH = "model_artifacts/2025_02_09_run_001/model_epoch_5.pth"
