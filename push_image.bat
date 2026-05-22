749cf5dba642f4aeabe55a4cfa5745840eac548e
Nathan Crock <mathnathan@users.noreply.github.com>
2026-05-21 20:44:54 -0700

    cleaning up main

--- push_image.bat
+++
@@ -1,2 +0,0 @@
-docker tag bridge us-central1-docker.pkg.dev/bridge-457501/ml-training-images/finetuning
-docker push us-central1-docker.pkg.dev/bridge-457501/ml-training-images/finetuning
--- start_local.bat
+++
@@ -1,1 +0,0 @@
-docker run --env-file ./.env bridge
